98f7bdf8f3f671988a2a2551f687abe2fdb78a3f
JESSICA PALOMA SOUSA CARDOSO <jcardoso@LIGETI.LAB-POS>
2018-05-15 17:44:32 -0300

    atualização do examplo

--- inst/doc/example.R
+++ inst/doc/example.R
@@ -4,37 +4,57 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ------------------------------------------------------------------------
-#install.packages('tuneR')
+## ---- echo=FALSE---------------------------------------------------------
 library(MFCC)
-library(tuneR)
-
-#Carregar arquivo de áudio exemplo
-sound.data <- MFCC::sound.data
-sound <- sound.data@left #valores do arquivo de áudio
-sample.rate <- sound.data@samp.rate #sample rate do arquivo de áudio
-sound.time <- 0:(length(sound)-1)/sample.rate
+ls("package:MFCC")
 
 ## ----fig.height = 2, fig.width = 7, fig.align = "center"-----------------
-library(ggplot2)
+library(MFCC) #carregar pacote do MFCC
+library(ggplot2) #biblioteca para gráficos
 
+#Obter arquivo de áudio exemplo
+sound.data <- MFCC::sound.data
+#Com o pacote tuneR, pode-se carregar um arquivo do disco da seguinte forma: (descomentar abaixo)
+#sound.data <- tuneR::readWave('audio.wav', from = 0, to = 3.5, units = "seconds")
+sound <- sound.data@left #valores do arquivo de áudio
+sample.rate <- sound.data@samp.rate #sample rate do arquivo de áudio
+
+#Exibir arquivo de áudio
+sound.time <- 0:(length(sound)-1)/sample.rate #tempo em segundos
+
+#criar estrutura contendo o x e y do gráfico
 data.raw <- data.frame(x = sound.time, y = sound)
 
-ggplot(data.raw, aes(x, y)) + geom_line() +
+#Exibir gráfico
+p <- ggplot(data.raw, aes(x, y)) + geom_line() +
   xlab("Tempo (s)") + ylab("Amplitude") #+
 
+
+## ---- fig.width = 7.25, fig.align = "center", echo = FALSE, message=FALSE----
+plotly::ggplotly(p)
+
+## ------------------------------------------------------------------------
+fft.npoints <- 512 #números de pontos considerados para o cálculo da fft 
+#help(nfft) #para mais informações
+freq.lower <- 0 #frequência mínima em hertz considerada
+freq.upper <- sample.rate / 2 #frequência máxima em hertz considerada
+num.filters <- 40 #número filtros considerados para o filterbank
 
 ## ----fig.height = 2, fig.width = 7, fig.align = "center"-----------------
 
 emphasized_signal <- apply_preemphasis(sound, 0.97)
 
+#criar estrutura contendo o x e y do gráfico
 data.emphasized <- data.frame(x = sound.time, y = emphasized_signal)
 
 #Exibição
-ggplot(data.emphasized, aes(x, y)) + geom_line() +
+p <- ggplot(data.emphasized, aes(x, y)) + geom_line() +
   xlab("Tempo (s)") + ylab("Amplitude")
 
-## ------------------------------------------------------------------------
+## ---- fig.width = 7.25, fig.align = "center", echo = FALSE, message=FALSE----
+plotly::ggplotly(p)
+
+## ---- echo = FALSE-------------------------------------------------------
 fft.npoints <- 512 #números de pontos considerados para o cálculo da fft 
 #help(nfft) #para mais informações
 freq.lower <- 0 #frequência mínima em hertz considerada
@@ -57,33 +77,6 @@ filter.banks <- power.frames %*% t(fbanks)
 filter.banks[filter.banks == 0] <- .Machine$double.eps #substituir os zeros para evitar problemas com log
 filter.banks <- 20 * log10(filter.banks)
 
-## ----fig.height = 2, fig.width = 7, fig.align = "center"-----------------
-x <- seq(from = freq.lower, to = freq.upper, length.out = ncol(fbanks)) %>% rep(num.filters)
-y <- t(fbanks)
-y %<>% as.data.frame() %>% tidyr::gather()
-
-data <- data.frame(x = x, values = y$value, filters = y$key)
-
-ggplot(data, aes(x, values, colour = filters)) +
-  geom_line() + xlab("Frequência") + ylab("Amplitude") + theme(legend.position="none") +
-  scale_x_continuous(expand = c(0, 0)) + 
-  scale_y_continuous(expand = c(0, 0))
-
-## ----fig.height = 2, fig.width = 7, fig.align = "center"-----------------
-
-#Organizar dado para melhor visualização
-fbanks.spec <- reshape2::melt(filter.banks)
-fbanks.spec$Var1 <- fbanks.spec$Var1 / 100 
-fbanks.spec$Var2 <- fbanks.spec$Var2 / 10 
-
-ggplot(fbanks.spec, aes(Var1,Var2)) + geom_raster(aes(fill = value)) +
-  scale_fill_gradientn(colours = rainbow(10)) +
-  xlab("Tempo (s)") + ylab("Frequência (kHz)") + ggtitle("Espectograma do sinal") +
-  scale_x_continuous(expand = c(0, 0)) + 
-  scale_y_continuous(expand = c(0, 0)) 
-
-
-## ------------------------------------------------------------------------
 #5. Obter a DCT do log das filterbank energies.
 mfcc <- t(apply(filter.banks, 1, function(x) apply_dct(x)))
 
@@ -91,20 +84,58 @@ mfcc <- t(apply(filter.banks, 1, function(x) apply_dct(x)))
 mfcc <- mfcc[, 2:13]
 
 ## ----fig.height = 2, fig.width = 7, fig.align = "center"-----------------
+
+#Organizar dado para melhor visualização
+x <- seq(from = freq.lower, to = freq.upper, length.out = ncol(fbanks)) %>% rep(num.filters)
+y <- t(fbanks)
+y %<>% as.data.frame() %>% tidyr::gather()
+
+data <- data.frame(x = x, values = y$value, filters = y$key)
+
+#Exibir espectograma
+p <- ggplot(data, aes(x, values, colour = filters)) +
+  geom_line() + xlab("Frequência") + ylab("Amplitude") + theme(legend.position="none") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0))
+
+## ---- fig.width = 7.25, fig.align = "center", echo = FALSE, message=FALSE----
+plotly::ggplotly(p)
+
+## ----fig.height = 2, fig.width = 7, fig.align = "center"-----------------
+
+#Organizar dado para melhor visualização
+fbanks.spec <- reshape2::melt(filter.banks)
+fbanks.spec$Var1 <- fbanks.spec$Var1 / 100
+fbanks.spec$Var2 <- fbanks.spec$Var2 / 10
+
+#Exibir espectograma
+p <- ggplot(fbanks.spec, aes(Var1,Var2)) + geom_raster(aes(fill = value)) +
+  scale_fill_gradientn(colours = rainbow(10)) +
+  xlab("Tempo (s)") + ylab("Frequência (kHz)") + ggtitle("Espectograma do sinal") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0))
+
+
+## ---- fig.width = 7.25, fig.align = "center", echo = FALSE, message=FALSE----
+plotly::ggplotly(p)
+
+## ----fig.height = 2, fig.width = 7, fig.align = "center"-----------------
 #Aplicar o sinusoidal liftering aos MFCCs
 mfcc.lift <- apply_lifter(mfcc)
 
-
 #Organizar dado para melhor visualização
 mfccs.spec <- reshape2::melt(mfcc.lift)
-mfccs.spec$Var1 <- mfccs.spec$Var1 / 100 
+mfccs.spec$Var1 <- mfccs.spec$Var1 / 100
 
 #Espectograma do MFCCs
-ggplot(mfccs.spec, aes(Var1,Var2, fill=value)) + geom_raster(aes(fill = value)) +
+p <- ggplot(mfccs.spec, aes(Var1,Var2, fill=value)) + geom_raster(aes(fill = value)) +
   scale_fill_gradientn(colours = rainbow(10)) +
   xlab("Tempo (s)") + ylab("Coeficientes das MFCCs") + ggtitle("MFCCs") +
-  scale_x_continuous(expand = c(0, 0)) + 
-  scale_y_continuous(expand = c(0, 0)) 
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0))
 
 
+
+## ---- fig.width = 7.25, fig.align = "center", echo = FALSE, message=FALSE----
+plotly::ggplotly(p)
 
