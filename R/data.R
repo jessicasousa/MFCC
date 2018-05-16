60717d267ecedabf4293ed7a7912f2bbc4891eda
Jessica <jessicacardosops@gmail.com>
2018-05-16 13:24:35 -0300

    update description

--- R/data.R
+++ R/data.R
@@ -1,4 +1,6 @@
 #' Amostra de áudio para teste
+#' 
+#' Corresponde aos 3,5 segundos do arquivo chamado OSR_us_000_0010_8k.wav
 #'
-#' @source \url{http://www.diamondse.info/}
+#' @source \url{http://www.voiptroubleshooter.com/open_speech/american.html}
 "sound.data"
